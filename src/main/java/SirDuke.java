59d64bae486d94be075e21a8fe70aebc58e478e7
noahang <noahangshihern@gmail.com>
2025-01-24 23:42:48 +0800

    Create SirDuke.start() method, add Echo functionality, fix style, remove logo
    -SirDuke.start() method created to contain welcome message printing and user input reading from console
    
    -SirDuke.start() method also contains Echo functionality, and calls SirDuke.sayBye() when "bye" command is received
    
    -horizontalLine variable is a constant, so it's variable name has been changed to HORIZONTAL_LINE
    -print messages have been updated to follow style guide for split strings
    -logo variable has been removed as it is no longer necessary

--- src/main/java/SirDuke.java
+++ src/main/java/SirDuke.java
@@ -1,23 +1,43 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class SirDuke {
-    static final String horizontalLine =
+    static final String HORIZONTAL_LINE =
             "____________________________________________________________";
+
+    /**
+     * Prints exit message and exits.
+     */
     public static void sayBye() {
-        System.out.println("Godspeed.\n" +
-                horizontalLine);
+        System.out.println(HORIZONTAL_LINE + "\n"
+                + "Godspeed.\n"
+                + HORIZONTAL_LINE);
+        System.exit(0);
+    }
+
+    /**
+     * Prints welcome message and allows the SirDuke chatbot to start receiving commands.
+     * Reads user input from console and interprets them as commands.
+     */
+    public static void start() {
+        System.out.println(HORIZONTAL_LINE + "\n"
+                + "It's a pleasure to meet you. My name is Sir Duke Ellington.\n"
+                + "What can I do you for?\n"
+                + HORIZONTAL_LINE);
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String command = scanner.nextLine();
+            if (Objects.equals(command, "bye")) {
+                SirDuke.sayBye();
+            } else {
+                System.out.println(HORIZONTAL_LINE + "\n"
+                        + command + "\n"
+                        + HORIZONTAL_LINE);
+            }
+        }
     }
     public static void main(String[] args) {
-
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(horizontalLine + "\n" +
-                "It's a pleasure to meet you. My name is Sir Duke Ellington.\n" +
-                "What can I do you for?\n" +
-                horizontalLine);
-
-        SirDuke.sayBye();
+        SirDuke.start();
     }
 }
 

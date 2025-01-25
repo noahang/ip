0b3411ec2465ef89265e04c30b1ff5de265ebd16
noahang <noahangshihern@gmail.com>
2025-01-26 01:12:16 +0800

    Revert "Add Task.java, ToDoList.java, Format.java"
    This reverts commit 527cb777b4491bf3e59eb245e120e8dd67d03ecc.

--- src/main/java/SirDuke.java
+++ src/main/java/SirDuke.java
@@ -1,46 +1,38 @@
+import java.util.Objects;
 import java.util.Scanner;
-import backend.ToDoList;
-import frontend.Format;
 public class SirDuke {
-    
+    static final String HORIZONTAL_LINE =
+            "____________________________________________________________";
+
     /**
      * Prints exit message and exits.
      */
     public static void sayBye() {
-        System.out.println(Format.HORIZONTAL_LINE + "\n"
+        System.out.println(HORIZONTAL_LINE + "\n"
                 + "Godspeed.\n"
-                + Format.HORIZONTAL_LINE);
+                + HORIZONTAL_LINE);
         System.exit(0);
     }
 
     /**
-     * Prints welcome message and allows the SirDuke chatbot to start receiving commands
-     * Reads user input from console and interprets them as commands
-     * Executes commands accordingly
+     * Prints welcome message and allows the SirDuke chatbot to start receiving commands.
+     * Reads user input from console and interprets them as commands.
      */
     public static void start() {
-
-        ToDoList list = new ToDoList();
-
-        System.out.println(Format.HORIZONTAL_LINE + "\n"
+        System.out.println(HORIZONTAL_LINE + "\n"
                 + "It's a pleasure to meet you. My name is Sir Duke Ellington.\n"
                 + "What can I do you for?\n"
-                + Format.HORIZONTAL_LINE);
+                + HORIZONTAL_LINE);
 
         Scanner scanner = new Scanner(System.in);
-
         while(true) {
             String command = scanner.nextLine();
-            switch (command) {
-                case "bye":
-                    SirDuke.sayBye();
-                    break;
-                case "list":
-                    list.showList();
-                    break;
-                default:
-                    list.createTask(command);
-                    break;
+            if (Objects.equals(command, "bye")) {
+                SirDuke.sayBye();
+            } else {
+                System.out.println(HORIZONTAL_LINE + "\n"
+                        + command + "\n"
+                        + HORIZONTAL_LINE);
             }
         }
     }

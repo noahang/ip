b8bba74acea029bc680354010c6bcb9c9c9622dc
noahang <noahangshihern@gmail.com>
2025-01-30 14:48:09 +0800

    edit Task.java class
    add javadoc for for Task class
    taskName member changed to string description
    status member changed to boolean isDone
    both members made protected
    getTaskName() method edited accordingly

--- src/main/java/backend/Task.java
+++ src/main/java/backend/Task.java
@@ -1,18 +1,21 @@
 package backend;
 
 public class Task {
-    String taskName;
-    String status;
-    Task(String taskName) {
-        this.taskName = taskName;
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Represents a task. A <code>Task<code> object has
+     * a <code>description<code> String member that is the name of the task
+     * a <code>isDone<code> boolean member that represents whether the task is completed or not
+     */
+    Task(String description) {
+        this.description = description;
+        this.isDone = false;
     }
-
-
+    
     public String getTaskName() {
-        return taskName;
+        return description;
     }
 
-    public String getStatus() {
-        return this.status;
-    }
 }

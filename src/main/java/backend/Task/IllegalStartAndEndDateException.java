0887b4eda54601439e0de4977c4975e9831edc73
noahang <noahangshihern@gmail.com>
2025-01-31 01:05:18 +0800

    add IllegalStartAndEndDateException.java

---
+++ src/main/java/backend/Task/IllegalStartAndEndDateException.java
@@ -0,0 +1,23 @@
+package backend.Task;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * An exception to be thrown when the startTime is after the endTime
+ */
+public class IllegalStartAndEndDateException extends IllegalArgumentException {
+
+    LocalDate startTime;
+    LocalDate endTime;
+    IllegalStartAndEndDateException(LocalDate startTime, LocalDate endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+    @Override
+    public String toString() {
+        return "The start date: "
+                + this.startTime.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+                + ", cannot be later than the end date: "
+                + this.endTime.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+}
